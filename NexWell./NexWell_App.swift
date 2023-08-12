943b9a009a9421122955a3e43acdaf4b2c96b679
Keoni <keoni319@gmail.com>
2023-08-12 18:34:42 +0800

    Initial Commit

---
+++ NexWell./NexWell_App.swift
@@ -0,0 +1,20 @@
+//
+//  NexWell_App.swift
+//  NexWell.
+//
+//  Created by Keoni Li on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NexWell_App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
